cb99c91fc25fe5cf870c470a3074c3f2f292f8fb
Alperen Rüzgar Erbosnalı <97199520+ARuzgar@users.noreply.github.com>
2024-11-12 16:47:25 +0300

    P0 - Ex02 is NOT done lol.

--- Python - 0 - Starting/Subjects/Ex 02/find_ft_type.py
+++ Python - 0 - Starting/Subjects/Ex 02/find_ft_type.py
@@ -1,3 +1,6 @@
-def all_thing_is_obj(obj: any) -> int:
-    print(f"The object type is: {type(obj)}")
+def all_thing_is_obj(object: any) -> int:
+    try:
+        print(f"{type(object).__name__} : {type(object)}")
+    except TypeError:
+        print("Type not found")
     return 42
