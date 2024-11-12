8820c6836d631cd8d5b7ddfb1ee64470827241f1
Alperen Rüzgar Erbosnalı <97199520+ARuzgar@users.noreply.github.com>
2024-11-12 17:18:55 +0300

    P0 - Ex renaming. Got rid of space.

--- Python - 0 - Starting/Subjects/Ex 02/find_ft_type.py
+++
@@ -1,6 +0,0 @@
-def all_thing_is_obj(object: any) -> int:
-    try:
-        print(f"{type(object).__name__} : {type(object)}")
-    except TypeError:
-        print("Type not found")
-    return 42
